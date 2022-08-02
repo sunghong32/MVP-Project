3e2c8c9db4eaf5dc9ee4f84e72632ba5f4b2c636
민성홍 <sunghong32@naver.com>
2022-08-02 21:04:54 +0900

    [Movie] Xcode Project에서 SwiftLint 설정하기
    (cherry picked from commit d09f85025dd0ee949370c7432fb1abbda7594136)

---
+++ MovieTests/MovieTests.swift
@@ -0,0 +1,32 @@
+//
+//  MovieTests.swift
+//  MovieTests
+//
+//  Created by 민성홍 on 2022/08/02.
+//
+
+import XCTest
+@testable import Movie
+
+class MovieTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
