d09f85025dd0ee949370c7432fb1abbda7594136
민성홍 <sunghong32@naver.com>
2022-08-02 21:04:54 +0900

    [Movie] Xcode Project에서 SwiftLint 설정하기

---
+++ Movie/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by 민성홍 on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
